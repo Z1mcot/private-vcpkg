badbbaea25982c2b4facf43e21b5285a976cfe8a
Ryczard Diubko <rechardzubko@gmail.com>
2026-03-21 13:13:34 +0300

    updated database versions

--- ports/tgbot-lib/portfile.cmake
+++ ports/tgbot-lib/portfile.cmake
@@ -1,9 +1,9 @@
-vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Z1mcot/tgbot-lib 
-    REF v1.0.3
+    REF v1.0.4
     SHA512 2be8c3f365d1e35400f044aaa5aef79ee9e190e68f800187dafec2174e1458e4686094ba55e9a5ed44cffc03936991daf06e97dbbe3b97ae7f21d400c29707e2
     HEAD_REF main
 )
